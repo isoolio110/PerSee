b556e538e41d98f6fe6fd2280f7de5b3e529badd
isoolio110 <kim.isul@gmail.com>
2015-04-30 14:01:32 -0400

    Adds route for generating receipts in parties controller

--- controllers/parties_controller.rb
+++ controllers/parties_controller.rb
@@ -38,5 +38,51 @@ class PartiesController < ApplicationController
     redirect "/parties"
   end
 
+  get '/:id/receipt' do
+    @party = Party.find(params[:id])
+    @orders = Order.where(party_id: params[:id])
+    CSV.open("/receipt.csv", "w") do |csv|
+      csv << ["Per See Receipt"]
+      csv << ["Thank Your for Dining!"]
+      csv << [nil,nil]
+      csv << [nil,nil]
+      csv << [nil,nil]
+      csv << ["Menu Item", "Price"]
+    end
+
+    CSV.open("/receipt.csv", "a") do |csv|
+      @orders.each  do |order|
+        csv << [order.food.name, order.food.price]
+      end
+      end
+
+      all_prices = @orders.map do |order|
+        order.food.price
+      end
+
+      sum_prices = all_prices.inject{|sum, x| sum + x}
+
+      CSV.open("/receipt.csv", "a") do |csv|
+        csv << ["Subtotal", sum_prices]
+        csv << ["Taxes", sum_prices * 0.0875]
+        csv << ["Total", sum_prices * 1.0875]
+        csv << [nil,nil]
+        csv << [nil,nil]
+        csv << [nil,nil]
+        csv << ["Suggested Gratuity (15%)",sum_prices * 1.0875 * 0.15 ]
+        csv << ["Suggested Gratuity (20%)",sum_prices * 1.0875 * 0.20 ]
+        csv << ["Suggested Gratuity (25%)",sum_prices * 1.0875 * 0.25 ]
+    end
+
+    @receipt = CSV.read("/receipt.csv")
+
+    prices = @receipt.map do |row|
+      row[1].to_f
+    end
+
+    @total = prices.inject {|sum, x| sum + x}
+    erb :"receipt/index"
+
+  end
 
 end
